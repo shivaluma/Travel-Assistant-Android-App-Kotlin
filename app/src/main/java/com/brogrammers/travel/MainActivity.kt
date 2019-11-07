c82b4fcb84d7abac1a500dbe502d8e867096afb0
shivaluma <shivaluma@gmail.com>
2019-11-07 10:49:54 +0700

    Add login template and login layout

--- app/src/main/java/com/brogrammers/travel/MainActivity.kt
+++ app/src/main/java/com/brogrammers/travel/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.brogrammers.travel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brogrammers.travel.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
