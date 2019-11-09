a1f70d8fce34d4e9a8c6c85b1bc8c74a8256756f
crepp5299 <tan.thai.2t@gmail.com>
2019-11-09 21:28:39 +0700

    + Add facebook login

--- app/src/main/java/com/brogrammers/travel/LoginActivity.kt
+++ app/src/main/java/com/brogrammers/travel/LoginActivity.kt
@@ -10,7 +10,22 @@ import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import bolts.Task
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.gson.JsonObject
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.layout_login.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -19,14 +34,28 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
     val BASE_URL = "http://35.197.153.192:3000"
+    var cbManager : CallbackManager ?= null
+    private lateinit var firebaseAuth: FirebaseAuth
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var mGoogleSignInOptions: GoogleSignInOptions
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        val sharePref : SharedPreferences = getSharedPreferences("logintoken", Context.MODE_PRIVATE)
+           val editor = sharePref.edit()
+
+
         setContentView(R.layout.activity_login)
 
         btnLogin.setOnClickListener {
@@ -49,10 +78,7 @@ class LoginActivity : AppCompatActivity() {
             jsonObject.addProperty("emailPhone", emailPhone)
             jsonObject.addProperty("password", password)
 
-            val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+
 
             val service = retrofit.create(ApiServiceLogin::class.java)
 
@@ -80,6 +106,56 @@ class LoginActivity : AppCompatActivity() {
             })
         }
 
+        btnFacebookInterface.setOnClickListener {
+            btnLoginFacebook.performClick()
+        }
+
+        btnLoginFacebook.setOnClickListener {
+
+            cbManager = CallbackManager.Factory.create()
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"))
+            LoginManager.getInstance().registerCallback(cbManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult?) {
+                    Log.d("Fbtoken", result!!.accessToken.token)
+                    val jsonObject = JsonObject()
+                    jsonObject.addProperty("accessToken", result.accessToken.token)
+                    val service = retrofit.create(ApiServiceFBLogin::class.java)
+                    val call = service.postData(jsonObject)
+                    call.enqueue(object : Callback<PostResponseFBLogin> {
+                        override fun onFailure(call: Call<PostResponseFBLogin>, t: Throwable) {
+                            Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
+                        }
+
+                        override fun onResponse(
+                            call: Call<PostResponseFBLogin>,
+                            response: Response<PostResponseFBLogin>
+                        ) {
+                            Log.d("fbmesg", response.message())
+                            if (response.code() != 200) {
+                                Toast.makeText(applicationContext, "Đăng nhập thất bại!", Toast.LENGTH_LONG).show()
+                            }
+                            else {
+                                Toast.makeText(applicationContext, "Đăng nhập thành công!", Toast.LENGTH_LONG).show()
+                                updateTokenToStorage(response.body()!!.token)
+                                startActivity(Intent(applicationContext,NavigationBottomActivity::class.java))
+                                finish()
+                            }
+                        }
+                    })
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+            })
+        }
+
+
+
     }
 
 
@@ -90,7 +166,17 @@ class LoginActivity : AppCompatActivity() {
                 editTextPassword.setText(data?.getStringExtra("passwrd"))
             }
         }
+        if (requestCode == 69) {
+            val task: com.google.android.gms.tasks.Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
         super.onActivityResult(requestCode, resultCode, data)
+        cbManager?.onActivityResult(requestCode,resultCode,data)
     }
 
     fun viewRegisterClicked(view: View) {
@@ -104,6 +190,54 @@ class LoginActivity : AppCompatActivity() {
         editor.putString("token", token)
     }
 
+    fun checkTokenAlive(token: String) {
+        
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val jsonObject = JsonObject()
+                jsonObject.addProperty("accessToken", acct.idToken.toString())
+                val service = retrofit.create(ApiServiceGGLogin::class.java)
+                val call = service.postData(jsonObject)
+
+                call.enqueue(object : Callback<PostResponseGGLogin> {
+
+                    override fun onFailure(call: Call<PostResponseGGLogin>, t: Throwable) {
+                        Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onResponse(
+                        call: Call<PostResponseGGLogin>,
+                        response: Response<PostResponseGGLogin>
+                    ) {
+                        if (response.message() == "Not Found") {
+                            Toast.makeText(applicationContext, "Đăng nhập thất bại!", Toast.LENGTH_LONG).show()
+                        }
+                        else {
+                            Toast.makeText(applicationContext, "Đăng nhập thành công!", Toast.LENGTH_LONG).show()
+                            updateTokenToStorage(response.body()!!.token)
+                            startActivity(Intent(applicationContext,NavigationBottomActivity::class.java))
+                            finish()
+                        }
+                    }
+                })
+            } else {
+                Toast.makeText(this, "Google sign in failed :(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun configureGoogleSignIn() {
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+    }
+
     private interface ApiServiceLogin {
         @POST("/user/login")
         fun postData(
@@ -111,7 +245,23 @@ class LoginActivity : AppCompatActivity() {
         ): Call<PostResponseLogin>
     }
 
+    private interface ApiServiceFBLogin {
+        @POST("/user/login/by-facebook")
+        fun postData(
+            @Body body: JsonObject
+        ): Call<PostResponseFBLogin>
+    }
+
+    private interface ApiServiceGGLogin {
+        @POST("/user/login/by-google")
+        fun postData(
+            @Body body: JsonObject
+        ): Call<PostResponseGGLogin>
+    }
+
+
+
     data class PostResponseLogin(val message:String, val userId:Int, val token:String)
-
-
+    data class PostResponseFBLogin(val message:String, val avatar:Int, val fullname:String, val token:String)
+    data class PostResponseGGLogin(val message:String, val avatar:Int, val fullname:String, val token:String)
 }
