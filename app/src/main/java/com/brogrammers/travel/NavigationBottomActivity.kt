ef8a307ee8a8d91c94c8ff060295d4ba3f8df465
nguyyentantai <mykendokai@gmail.com>
2019-11-16 16:07:29 +0700

    Add history to navigation bottom

--- app/src/main/java/com/brogrammers/travel/NavigationBottomActivity.kt
+++ app/src/main/java/com/brogrammers/travel/NavigationBottomActivity.kt
@@ -1,5 +1,6 @@
 package com.brogrammers.travel
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,16 +15,18 @@ class NavigationBottomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_user
+                R.id.navigation_home,R.id.navigation_history, R.id.navigation_dashboard, R.id.navigation_user
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
 }
